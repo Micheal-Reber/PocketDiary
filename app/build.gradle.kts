e63f096b13080a64790ed8cee234785ac35cf00b
Micheal-Reber <junxuanwang948@gmail.com>
2026-08-23 22:44:07 +0800

    版本号升级 v1.2 (versionCode 2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.example.diary"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
     }
